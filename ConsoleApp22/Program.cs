b3cc81f824a4e627591a3e1f28e1e23f5cd12775
admin <admin@CHETUIWK1007>
2022-06-25 17:24:35 +0530

    Add project files.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+       
+            public void calculate_sum(int n1, int n2)
+            {
+                int result = n1 + n2;
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+            static void Main(string[] args)
+            {
+
+                Program objpg = new Program();
+                objpg.calculate_sum(10, 25);
+
+            }
+        }
+
+    }
+    
+
